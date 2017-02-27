6069877268995d6c55c8130c0c6b7976253df022
EMPYREA <world.color.1994@gmail.com>
2017-02-27 15:03:03 +0900

    day 1 の終了地点
    スプリングコースの１日目終了地点の状態です。

---
+++ day1/js/day1.js
@@ -0,0 +1,19 @@
+﻿console.log("テスト");
+
+var elements = document.getElementByClassName( "main" ) ;
+
+elements.insertAdjacentHTML('afterbegin','<p>test</p>');
+
+function sample(){
+	console.log("これは、javascriptsのテストです")
+}
+var Cat = function (name){
+	this.name = name;
+	this.run = function (){
+		console.log(this.name + "が走る");
+	}
+}
+var tama = new Cat('タマ');
+tama.run();
+console.log(tama.name);
+
